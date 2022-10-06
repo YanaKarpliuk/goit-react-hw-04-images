a74689c058b5032c50a2bc496651af47e4e462b1
Yana Karpliuk <yanakarpliuk99@gmail.com>
2022-10-06 14:45:24 +0300

    removed unused var

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -2,8 +2,6 @@ import { Component } from 'react';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleyItem';
 import styleImageGallery from './imageGallery.module.css';
 import PropTypes from 'prop-types';
-import Modal from 'components/Modal/Modal';
-
 export default class ImageGallery extends Component {
   render() {
     return (
